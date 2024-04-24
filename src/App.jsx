52ded7bdcc0866fcfd2d55459ad6194cd5f250c8
Sujan <mail.thapasujan@gmail.com>
2024-04-25 09:59:34 +1000

    Blank form and table component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Form } from "./components/Form";
+import { Table } from "./components/Table";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    </>
-  )
+    <div className="container pt-5">
+      <h1 className="mt-4 mb-4">Bucket List Manager</h1>
+      <Form />
+      <Table/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Table.jsx
@@ -0,0 +1,87 @@
+import React, { useState } from "react";
+
+export const Table = () => {
+  return (
+    <div className="card border border-white shadow-lg p-4 bg-transparent blurred-background">
+      <div className="row">
+        <div className="col-md">
+          <h3 className="text-center">Bucket List</h3>
+          <hr />
+          <table className="table table-hover table-transparent">
+            <thead>
+              <tr>
+                <td>#</td>
+                <td>Title</td>
+                <td>Location</td>
+                <td>Category</td>
+                <td>Cost</td>
+              </tr>
+            </thead>
+            <tbody id="entry" className="table-group-divider">
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="col-md">
+          <h3 className="text-center">Ticked List</h3>
+          <hr />
+          <table className="table table-hover table-transparent">
+            <thead>
+              <tr>
+                <td>#</td>
+                <td>Title</td>
+                <td>Location</td>
+                <td>Category</td>
+                <td>Cost</td>
+              </tr>
+            </thead>
+            <tbody id="checked" className="table-group-divider">
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+              <tr>
+                <td>1.</td>
+                <td>Bungee Jump</td>
+                <td>Bhote Koshi</td>
+                <td>Adventure</td>
+                <td>10000</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+};
